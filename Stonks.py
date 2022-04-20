7ae2208cd371d92183babfae3e4dffbc843395ca
Michael Nejaime <mnejaime@icloud.com>
2022-04-20 18:00:42 -0400

    added extra analysis functions, updated stocks.py

--- Stonks.py
+++ Stonks.py
@@ -10,10 +10,16 @@ from authenticate import *
 # import numpy as np
 # import time
 # from stockAnalysisFunctions import *
-getStockPrice('AAPL')
 
+# Testing functions with AAPL
+print(str(getStockPrice('AAPL')))
+print(str(getOpenPrice('AAPL')))
 
+# Creates generic list of stocks that user can add to
+listOfStocks = ['AAPL', 'SPY', 'MSFT', 'TSLA']
 
-
+# Command-line tool to see where stocks opened, and where they are now
+for x in listOfStocks:
+    print(x + " opened at " + str(getOpenPrice(x)) + ", and is now at " + str(getStockPrice(x)))
 
 
--- stockAnalysisFunctions.py
+++ stockAnalysisFunctions.py
@@ -1,10 +1,23 @@
 import authenticate
 import yfinance as yf
+from datetime import date
 from authenticate import *
 
+
+# Gets today's date and formats it in a way that is used by yfinance
+todaysDate = date.today()
+todaysDate = todaysDate.strftime("%Y-%m-%d")
+
 def getStockPrice(ticker):
+    '''gets current price of a stock'''
     tick=yf.Ticker(ticker)
     data=tick.history()
     lastPrice=data['Close'].iloc[-1]
-    print(lastPrice)
+    return lastPrice
 
+def getOpenPrice(ticker):
+    '''gets opening price of a stock'''
+    tick = yf.Ticker(ticker)
+    data = tick.history()
+    openPrice = data['Open'][todaysDate]
+    return openPrice
