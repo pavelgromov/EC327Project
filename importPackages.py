42b5db6d14b6de1655c2b3619566097d2fd6f0d6
wackbeans <98371344+wackbeans@users.noreply.github.com>
2022-04-25 23:07:20 -0400

    finished import file

--- importPackages.py
+++ importPackages.py
@@ -3,3 +3,6 @@ import sys
 import subprocess
 
 subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'yfinance'])
+subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'numpy'])
+subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'pandas'])
+subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'tweepy'])
