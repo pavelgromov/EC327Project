ea277e88a13fdf1462678effdad14c51843d0bfb
pavelgromov <pavelg@bu.edu>
2022-04-28 19:30:02 -0400

    added pie chart and wordcloud

--- SentimentV3.py
+++ SentimentV3.py
@@ -1,12 +1,15 @@
 import os
 import csv
 import json
+import re
+import numbers
 ##from turtle import clear
 import matplotlib.pyplot as plot
 import requests
 import pandas as pd
 import tweepy as tw
 from datetime import datetime, timedelta
+from wordcloud import WordCloud
 import vaderSentiment
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 bearer_token = 'AAAAAAAAAAAAAAAAAAAAAIoZbgEAAAAA2ZKX19DgAuBJiiQqqkJHRkqil%2Fk%3Dt5TKQDRnV278J2CFWzaSDhPRrmS6Kn1akiYm72YtCFh7mV7Owm'
@@ -51,13 +54,47 @@ def get_tweets(stock):
     return result
     
 def getPolarity(tweets):
+    cleaning=tweets
     score=analyzer.polarity_scores(tweets)
     print(score)
     scores=score.values()
     scores_list=list(scores)
     negative=scores_list[0]
+    cleaning=clean(cleaning)
+    wordcloud=WordCloud().generate(cleaning)
+    wordcloud.to_file('stockWordCloud'+'.jpg')
+
     return scores_list
 
+def clean(tweets):
+    numbers = ['1','2','3','4','5','6','7','8','9','0']
+    dumbwords=["id","RT","text", "u"]
+    result= tweets.lower()
+    result = re.sub(r"https\S+", "", result)
+    result = re.sub(r"www.\S+", "", result)
+    #result = re.sub()
+    result= re.sub("@[A-Za-z0-9_]+","", result)
+    result= re.sub("#[A-Za-z0-9_]+","", result)
+    result= re.sub('[()!?]', ' ', result)
+    result= re.sub('\[.*?\]',' ', result)
+    results=re.sub("[0-9]"," ",result)
+    result= re.sub("[^a-z0-9]"," ", result)
+    stopwords = ["for", "on", "an", "a", "of", "and", "in", "the", "to", "from", "id","RT","text"]
+    result=[x for x in result if not x in numbers]
+    result= [w for w in result if not w in stopwords]
+    result= [w for w in result if not w in dumbwords]
+    result= " ".join(word for word in result)
+    result=[word for word in result if word not in dumbwords]
+    return tweets
+   
+       
+
+stock='APPL'
+tweets=get_tweets(stock)
+clean(tweets)
+
+
+'''
 def makePlot(stock):
     tweets=get_tweets(stock)
     score=getPolarity(tweets)
@@ -68,11 +105,11 @@ def makePlot(stock):
     ax1.pie(chunks,explode=sep,labels=label,autopct='%1.1f%%')
     ax1.axis('equal')
     plot.show()
-
+'''
     
 ##tweets=get_tweets()
-stock='TWTR'
-makePlot(stock)
+##stock='TWTR'
+##makePlot(stock)
 ##scores=getPolarity(tweets)
 ##print(scores)
 '''
--- StonksMain.py
+++ StonksMain.py
@@ -1,16 +1,20 @@
 import sys
-#from typing_extensions import Self
+# from typing_extensions import Self
 import matplotlib.pyplot as plot
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg 
+from matplotlib.figure import Figure
 from stockAnalysisFunctions import *
 from SentimentV3 import *
 from PyQt5.QtWidgets import QApplication, QDialog, QMainWindow, QTextEdit, QGraphicsScene, QGraphicsView
 from PyQt5 import QtCore, QtGui, QtWidgets, uic
+from PyQt5.QtGui import QPixmap
 import newGui
 import pandas as pd
 import numpy
+from PIL import Image
 
 
-class StonksGui(QtWidgets.QMainWindow, newGui.Ui_MainWindow):
+class StonksGui(QtWidgets.QMainWindow, newGui.Ui_MainWindow,QGraphicsView,QGraphicsScene):
     def __init__(self, parent=None):
         super(StonksGui, self).__init__(parent)
         self.setupUi(self)
@@ -47,22 +51,40 @@ class StonksGui(QtWidgets.QMainWindow, newGui.Ui_MainWindow):
         # self.label_6.setText(closePrice)
         self.label_8.setText(YearHigh)
         self.label_11.setText(YearLow)
-'''
+
     def makePlot(self):
+        Figure(figsize=(331,241),dpi=1)
         stock = self.lineEdit.text()
-        tweets=get_tweets(stock)
-        score=getPolarity(tweets)
-        label='Negative', 'Neutral', 'Postive'
-        chunks=[score[0]*100, score[1]*100, score[2]*100]
-        sep=(0,0,0.1)
-        fig1,ax1=plot.subplots()
-        ax1.pie(chunks,explode=sep,labels=label,autopct='%1.1f%%')
+        tweets = get_tweets(stock)
+        score = getPolarity(tweets)
+        label = 'Negative', 'Neutral', 'Postive'
+        chunks = [score[0]*100, score[1]*100, score[2]*100]
+        sep = (0, 0, 0.1)
+        fig1, ax1 = plot.subplots()
+        ax1.pie(chunks, explode=sep, labels=label, autopct='%1.1f%%')
         ax1.axis('equal')
-        ##plot.show()
-        self.graphicsView=plot.PlotWidget(self.centralwidget)
-'''
+        plot.savefig("stockfigure.jpg")
+        image=Image.open('stockfigure.jpg')
+        image_name='stockfigure.jpg'
+        wordcloudname='stockWordCloud.jpg'
+        pixmap=QPixmap(image_name)
+        pixmap2=QPixmap(wordcloudname)
+        self.label_12.setScaledContents(True)
+        self.label_12.setPixmap(pixmap)
+        self.label_13.setPixmap(pixmap2)
+
+    
+class MplCanvas(FigureCanvasQTAgg):
+
+    def __init__(self, parent=None, width=2, height=1, dpi=100):
+        fig = Figure(figsize=(width, height), dpi=dpi)
+        self.axes = fig.add_subplot(111)
+        super(MplCanvas, self).__init__(fig)
+
+
 def main():
     app = QApplication(sys.argv)
+    
     form = StonksGui()
     form.show()
     app.exec()
