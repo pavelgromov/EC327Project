b92bbcbd36f275e30f0c6d32e05fa61b2b67e4f6
wackbeans <98371344+wackbeans@users.noreply.github.com>
2022-05-02 01:40:27 -0400

    debugging import packages

--- importPackages.py
+++ importPackages.py
@@ -1,7 +1,7 @@
-
+import os
+os.system('cmd /c "python -m pip install --upgrade --force-reinstall pip"')
 import sys
 import subprocess
-
 subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'yfinance'])
 subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'numpy'])
 subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'pandas'])
@@ -9,15 +9,7 @@ subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'tweepy'])
 subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'vaderSentiment'])
 subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'wordcloud'])
 subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'pyqt5'])
-subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'sys'])
-subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'errno'])
-subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'configparser'])
-subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'PIL'])
-subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'json'])
-subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'os'])
-subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'os'])
-subprocess.check_call([sys.executable, '-m', 'pip', 'install', 're'])
-subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'numbers'])
 subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'matplotlib'])
 subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'requests'])
 subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'datetime'])
+subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'pillow'])
